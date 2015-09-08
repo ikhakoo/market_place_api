28525f5dd25e72836331fffb3e14034be04e05cb
ikhakoo <ikhakoo@gmail.com>
2015-09-08 00:45:25 -0400

    updating spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -17,7 +17,7 @@ describe Api::V1::UsersController do
     it { should respond_with 200 }
   end
 
-   describe "POST #create" do
+  describe "POST #create" do
 
     context "when is successfully created" do
       before(:each) do
@@ -37,8 +37,9 @@ describe Api::V1::UsersController do
       before(:each) do
         #notice I'm not including the email
         @invalid_user_attributes = { password: "12345678",
-                                     password_confirmation: "12345678" }
-        post :create, { user: @invalid_user_attributes }, format: :json
+                            password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes },
+                                format: :json
       end
 
       it "renders an errors json" do
@@ -46,7 +47,7 @@ describe Api::V1::UsersController do
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on why the user could not be created" do
+      it "renders the json errors on whye the user could not be created" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
@@ -56,12 +57,14 @@ describe Api::V1::UsersController do
   end
 
   describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+    end
 
     context "when is successfully updated" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,
-                         user: { email: "newmail@example.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "newmail@example.com" } },
+                         format: :json
       end
 
       it "renders the json representation for the updated user" do
@@ -74,9 +77,8 @@ describe Api::V1::UsersController do
 
     context "when is not created" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,
-                         user: { email: "bademail.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "bademail.com" } },
+                         format: :json
       end
 
       it "renders an errors json" do
@@ -94,12 +96,12 @@ describe Api::V1::UsersController do
   end
 
   describe "DELETE #destroy" do
-	  before(:each) do
-	    @user = FactoryGirl.create :user
-	    delete :destroy, { id: @user.id }, format: :json
-	  end
+    before(:each) do
+      @user = FactoryGirl.create :user
+      delete :destroy, { id: @user.id }, format: :json
+    end
 
-	  it { should respond_with 204 }
-	end
-	
+    it { should respond_with 204 }
+
+  end
 end
